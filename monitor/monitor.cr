140f879f34ed88768c8a1dd713204c1612dfe42c
Heaven31415 <Heaven31415@gmail.com>
2018-08-25 11:26:17 +0200

    Repair Monitor.

--- monitor/monitor.cr
+++ monitor/monitor.cr
@@ -1,5 +1,7 @@
+# TODO: get rid of monitor class methods
+
 module GLFW
-  class Monitor
+  struct Monitor
     getter ptr : LibGLFW::Monitor*
 
     def initialize(@ptr)
@@ -8,75 +10,75 @@ module GLFW
     def to_unsafe : LibGLFW::Monitor*
       @ptr
     end
+  end
 
-    def self.get_array : Array(Monitor)
-      monitors = Array(Monitor).new
-      monitors_ptr = LibGLFW.get_monitors(out count)
+  def self.get_monitors : Array(Monitor)
+    monitors = Array(Monitor).new
+    monitors_ptr = LibGLFW.get_monitors(out count)
 
-      i = 0
-      while i < count 
-        monitors << Monitor.new(monitors_ptr[i])
-        i += 1
+    i = 0
+    while i < count 
+      monitors << Monitor.new(monitors_ptr[i])
+      i += 1
+    end
+
+    monitors
+  end
+
+  def self.get_primary_monitor : Monitor
+    Monitor.new(LibGLFW.get_primary_monitor)
+  end
+
+  def self.get_monitor_pos(monitor : Monitor) : {x: Int32, y: Int32}
+    LibGLFW.get_monitor_pos(monitor.ptr, out xpos, out ypos)
+    {x: xpos, y: ypos}
+  end
+
+  def self.get_monitor_physical_size(monitor : Monitor) : {width: Int32, height: Int32}
+    LibGLFW.get_monitor_physical_size(monitor.ptr, out width, out height)
+    {width: width, height: height}
+  end
+
+  def self.get_monitor_name(monitor : Monitor) : String
+    String.new(LibGLFW.get_monitor_name(monitor.ptr))
+  end
+
+  @@callback : Proc(Monitor, Event::Monitor, Void)? = nil
+  def self.set_monitor_callback(&block : Monitor, Event::Monitor -> Void)
+    @@callback = block
+    LibGLFW.set_monitor_callback ->(monitor : LibGLFW::Monitor*, event : Int32) do
+      if cb = @@callback
+        cb.call(Monitor.new(monitor), Event::Monitor.new(event))
       end
+    end
+  end 
 
-      monitors
+  def self.get_video_modes(monitor : Monitor) : Array(VideoMode)
+    video_modes = Array(VideoMode).new
+    video_modes_ptr = LibGLFW.get_video_modes(monitor.ptr, out count)
+
+    i = 0
+    while i < count
+      video_modes << VideoMode.new(video_modes_ptr + i)
+      i += 1
     end
 
-    def self.get_primary : Monitor
-      Monitor.new(LibGLFW.get_primary_monitor)
-    end
+    video_modes
+  end
 
-    def self.get_position(monitor : Monitor) : {x: Int32, y: Int32}
-      LibGLFW.get_monitor_pos(monitor.ptr, out xpos, out ypos)
-      {x: xpos, y: ypos}
-    end
+  def self.get_video_mode(monitor : Monitor) : VideoMode
+    VideoMode.new(LibGLFW.get_video_mode(monitor.ptr))
+  end
 
-    def self.get_physical_size(monitor : Monitor) : {width: Int32, height: Int32}
-      LibGLFW.get_monitor_physical_size(monitor.ptr, out width, out height)
-      {width: width, height: height}
-    end
+  def self.set_gamma(monitor : Monitor, gamma : Float32)
+    LibGLFW.set_gamma(monitor.ptr, gamma)
+  end
 
-    def self.get_name(monitor : Monitor) : String
-      String.new(LibGLFW.get_monitor_name(monitor.ptr))
-    end
+  def self.get_gamma_ramp(monitor : Monitor) : GammaRamp
+    GammaRamp.new(LibGLFW.get_gamma_ramp(monitor.ptr))
+  end
 
-    @@callback : Proc(Monitor, Event::Monitor, Void)? = nil
-    def self.set_callback(&block : Monitor, Event::Monitor -> Void)
-      @@callback = block
-      LibGLFW.set_monitor_callback ->(monitor : LibGLFW::Monitor*, event : Int32) do
-        if cb = @@callback
-          cb.call(Monitor.new(monitor), Event::Monitor.new(event))
-        end
-      end
-    end 
-
-    def self.get_video_modes(monitor : Monitor) : Array(VideoMode)
-      video_modes = Array(VideoMode).new
-      video_modes_ptr = LibGLFW.get_video_modes(monitor.ptr, out count)
-
-      i = 0
-      while i < count
-        video_modes << VideoMode.new(video_modes_ptr + i)
-        i += 1
-      end
-
-      video_modes
-    end
-
-    def self.get_video_mode(monitor : Monitor) : VideoMode
-      VideoMode.new(LibGLFW.get_video_mode(monitor.ptr))
-    end
-
-    def self.set_gamma(monitor : Monitor, gamma : Float32)
-      LibGLFW.set_gamma(monitor.ptr, gamma)
-    end
-
-    def self.get_gamma_ramp(monitor : Monitor) : GammaRamp
-      GammaRamp.new(LibGLFW.get_gamma_ramp(monitor.ptr))
-    end
-
-    def self.set_gamma_ramp(monitor : Monitor, ramp : GammaRamp)
-      LibGLFW.set_gamma_ramp(monitor.ptr, ramp.ptr)
-    end
+  def self.set_gamma_ramp(monitor : Monitor, ramp : GammaRamp)
+    LibGLFW.set_gamma_ramp(monitor.ptr, ramp.ptr)
   end
 end
