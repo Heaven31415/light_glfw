c555fdb32767004e719085cf8f93699622127c98
Heaven31415 <Heaven31415@gmail.com>
2018-08-25 11:23:12 +0200

    Repair Image.

--- window/image.cr
+++ window/image.cr
@@ -1,31 +1,18 @@
 module GLFW
-  class Image
+  struct Image
     getter width : Int32
     getter height : Int32
-    getter pixels : Slice(UInt8)
+    getter pixels : Pointer(UInt8)
 
     def initialize(@width, @height, @pixels)
     end
 
     def to_unsafe : LibGLFW::Image*
-      image_ptr = Pointer(LibGLFW::Image).malloc
-      image = image_ptr.value
-      image.width = @width
-      image.height = @height
-      image.pixels = @pixels.to_unsafe
-      image_ptr
-    end
-
-    def width : Int32
-      @ptr.value.width
-    end
-
-    def height : Int32
-      @ptr.value.height
-    end
-
-    def pixels : Slice(UInt8)
-      Slice(UInt8).new(@ptr.value.pixels, @ptr.value.width * @ptr.value.height)
+      image = Pointer(LibGLFW::Image).malloc
+      image.value.width = @width
+      image.value.height = @height
+      image.value.pixels = @pixels
+      image
     end
   end
 end
