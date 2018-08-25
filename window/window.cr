a713272704df80535ed6b742c25198cc88fd4e0c
Heaven31415 <Heaven31415@gmail.com>
2018-08-25 18:13:53 +0200

    Inline window methods.

--- window/window.cr
+++ window/window.cr
@@ -1,4 +1,8 @@
 # todo: get rid of class methods for Window?
+# todo: I think I should handle errors with exceptions
+
+# todo: every method should be inlined and I should avoid to_unsafe implicit call
+# todo: every method which doesn't return anything interesing should have Nil return value
 
 module GLFW
   struct Window
@@ -12,117 +16,225 @@ module GLFW
     end
   end
 
-  def self.default_window_hints
+  # fun default_window_hints = glfwDefaultWindowHints : Void
+  @[AlwaysInline]
+  def self.default_window_hints : Nil
     LibGLFW.default_window_hints
   end
 
   # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
-  # GLFW::Hint => enum
-  # GLFW::Value
-  def self.window_hint(hint : Int32, value : Int32)
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : Bool) : Nil
+    LibGLFW.window_hint(hint, value == true ? LibGLFW::TRUE : LibGLFW::FALSE)
+  end
+
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : Int32) : Nil
     LibGLFW.window_hint(hint, value)
   end
 
-  def self.create_window(width : Int32, height : Int32, title : String, monitor : Monitor? = nil, share : Window? = nil) : Window
-    Window.new(LibGLFW.create_window(width, height, title, monitor, share))
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : ClientApi) : Nil
+    LibGLFW.window_hint(hint, value.value)
   end
 
-  def self.destroy_window(window : Window)
-    LibGLFW.destroy_window(window)
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : ContextRobustness) : Nil
+    LibGLFW.window_hint(hint, value.value)
   end
 
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : OpenGLProfile) : Nil
+    LibGLFW.window_hint(hint, value.value)
+  end
+
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : ContextReleaseBehavior) : Nil
+    LibGLFW.window_hint(hint, value.value)
+  end
+
+  # fun window_hint = glfwWindowHint(hint : Int32, value : Int32) : Void
+  @[AlwaysInline]
+  def self.window_hint(hint : Hint, value : ContextCreation) : Nil
+    LibGLFW.window_hint(hint, value.value)
+  end
+
+  # fun create_window = glfwCreateWindow(width : Int32, height : Int32, title : UInt8*, monitor : Monitor*, share : Window*) : Window*
+  @[AlwaysInline]
+  def self.create_window(width : Int32, height : Int32, title : String, monitor : Monitor? = nil, share : Window? = nil) : Window?
+    ptr = LibGLFW.create_window(width, height, title, monitor, share)
+    if ptr.null?
+      nil
+    else
+      Window.new(ptr)
+    end
+  end
+
+  # fun destroy_window = glfwDestroyWindow(window : Window*) : Void
+  @[AlwaysInline]
+  def self.destroy_window(window : Window) : Nil
+    LibGLFW.destroy_window(window.ptr)
+  end
+
+  # fun window_should_close = glfwWindowShouldClose(window : Window*) : Int32
+  @[AlwaysInline]
   def self.window_should_close(window : Window) : Bool
-    LibGLFW.window_should_close(window) == LibGLFW::TRUE ? true : false
+    LibGLFW.window_should_close(window.ptr) == LibGLFW::TRUE ? true : false
   end
 
-  def self.set_window_should_close(window : Window, value : Bool)
-    LibGLFW.set_window_should_close(window, value == true ? LibGLFW::TRUE : LibGLFW::FALSE)
+  # fun set_window_should_close = glfwSetWindowShouldClose(window : Window*, value : Int32) : Void
+  @[AlwaysInline]
+  def self.set_window_should_close(window : Window, value : Bool) : Nil
+    LibGLFW.set_window_should_close(window.ptr, value == true ? LibGLFW::TRUE : LibGLFW::FALSE)
   end
 
-  def self.set_window_title(window : Window, title : String) : Void
-    LibGLFW.set_window_title(window, title)
+  # fun set_window_title = glfwSetWindowTitle(window : Window*, title : UInt8*) : Void
+  @[AlwaysInline]
+  def self.set_window_title(window : Window, title : String) : Nil
+    LibGLFW.set_window_title(window.ptr, title)
   end
 
   # fun set_window_icon = glfwSetWindowIcon(window : Window*, count : Int32, images : Image*) : Void
-  def self.set_window_default_icon(window : Window) : Void
-    LibGLFW.set_window_icon(window, 0, nil)
-  end
-
-  def self.set_window_icon(window : Window, image : Image) : Void
-    LibGLFW.set_window_icon(window, 1, image)
+  @[AlwaysInline]
+  def self.set_window_icon(window : Window, images : Array(Image)?) : Nil
+    if images
+      LibGLFW.set_window_icon(window.ptr, images.size, images.to_unsafe)
+    else
+      LibGLFW.set_window_icon(window.ptr, 0, nil)
+    end
   end
 
   # fun get_window_pos = glfwGetWindowPos(window : Window*, xpos : Int32*, ypos : Int32*) : Void
+  @[AlwaysInline]
   def self.get_window_pos(window : Window) : {x: Int32, y: Int32}
-    LibGLFW.get_window_pos(window, out xpos, out ypos)
+    LibGLFW.get_window_pos(window.ptr, out xpos, out ypos)
     {x: xpos, y: ypos}
   end
 
   # fun set_window_pos = glfwSetWindowPos(window : Window*, xpos : Int32, ypos : Int32) : Void
-  def self.set_window_pos(window : Window, position : {x: Int32, y: Int32})
-    LibGLFW.set_window_pos(window, position[:x], position[:y])
+  @[AlwaysInline]
+  def self.set_window_pos(window : Window, position : {x: Int32, y: Int32}) : Nil
+    LibGLFW.set_window_pos(window.ptr, position[:x], position[:y])
   end
 
   # fun get_window_size = glfwGetWindowSize(window : Window*, width : Int32*, height : Int32*) : Void
+  @[AlwaysInline]
   def self.get_window_size(window : Window) : {width: Int32, height: Int32}
-    LibGLFW.get_window_size(window, out w, out h)
+    LibGLFW.get_window_size(window.ptr, out w, out h)
     {width: w, height: h}
   end
 
   # fun set_window_size_limits = glfwSetWindowSizeLimits(window : Window*, min_width : Int32, min_height : Int32, max_width : Int32, max_height : Int32) : Void
-  def self.set_window_size_limits(window : Window, min_width : Int32, min_height : Int32, max_width : Int32, max_height : Int32)
-    LibGLFW.set_window_size_limits(window, min_width, min_height, max_width, max_height)
+  @[AlwaysInline]
+  def self.set_window_size_limits(window : Window, min_width : Int32, min_height : Int32, max_width : Int32, max_height : Int32) : Nil
+    LibGLFW.set_window_size_limits(window.ptr, min_width, min_height, max_width, max_height)
   end
 
   # fun set_window_aspect_ratio = glfwSetWindowAspectRatio(window : Window*, numer : Int32, denom : Int32) : Void
-  def self.set_window_aspect_ratio(window : Window, numerator : Int32, denominator : Int32)
-    LibGLFW.set_window_aspect_ratio(window, numerator, denominator)
+  @[AlwaysInline]
+  def self.set_window_aspect_ratio(window : Window, numerator : Int32, denominator : Int32) : Nil
+    LibGLFW.set_window_aspect_ratio(window.ptr, numerator, denominator)
   end
 
   # fun set_window_size = glfwSetWindowSize(window : Window*, width : Int32, height : Int32) : Void
-  def self.set_window_size(window : Window, width : Int32, height : Int32)
-    LibGLFW.set_window_size(window, width, height)
+  @[AlwaysInline]
+  def self.set_window_size(window : Window, width : Int32, height : Int32) : Nil
+    LibGLFW.set_window_size(window.ptr, width, height)
   end
 
   # fun get_framebuffer_size = glfwGetFramebufferSize(window : Window*, width : Int32*, height : Int32*) : Void
+  @[AlwaysInline]
   def self.get_framebuffer_size(window : Window) : {width: Int32, height: Int32}
-    LibGLFW.get_framebuffer_size(window, out w, out h)
+    LibGLFW.get_framebuffer_size(window.ptr, out w, out h)
     {width: w, height: h}
   end
 
   # fun get_window_frame_size = glfwGetWindowFrameSize(window : Window*, left : Int32*, top : Int32*, right : Int32*, bottom : Int32*) : Void
+  @[AlwaysInline]
   def self.get_window_frame_size(window : Window) : {left: Int32, top: Int32, right: Int32, bottom: Int32}
-    LibGLFW.get_window_frame_size(window, out l, out t, out r, out b)
+    LibGLFW.get_window_frame_size(window.ptr, out l, out t, out r, out b)
     {left: l, top: t, right: r, bottom: b}
   end
 
   # fun iconify_window = glfwIconifyWindow(window : Window*) : Void
-  def self.iconify_window(window : Window)
-    LibGLFW.iconify_window(window)
+  @[AlwaysInline]
+  def self.iconify_window(window : Window) : Nil
+    LibGLFW.iconify_window(window.ptr)
   end
 
   # fun restore_window = glfwRestoreWindow(window : Window*) : Void
-  def self.restore_window(window : Window)
-    LibGLFW.restore_window(window)
+  @[AlwaysInline]
+  def self.restore_window(window : Window) : Nil
+    LibGLFW.restore_window(window.ptr)
   end
 
   # fun maximize_window = glfwMaximizeWindow(window : Window*) : Void
-  def self.maximize_window(window : Window)
-    LibGLFW.maximize_window(window)
+  @[AlwaysInline]
+  def self.maximize_window(window : Window) : Nil
+    LibGLFW.maximize_window(window.ptr)
   end
 
   # fun show_window = glfwShowWindow(window : Window*) : Void
-  def self.show_window(window : Window)
-    LibGLFW.show_window(window)
+  @[AlwaysInline]
+  def self.show_window(window : Window) : Nil
+    LibGLFW.show_window(window.ptr)
   end
 
   # fun hide_window = glfwHideWindow(window : Window*) : Void
-  def self.hide_window(window : Window)
-    LibGLFW.hide_window(window)
+  @[AlwaysInline]
+  def self.hide_window(window : Window) : Nil
+    LibGLFW.hide_window(window.ptr)
   end
 
   # fun focus_window = glfwFocusWindow(window : Window*) : Void
-  def self.focus_window(window : Window)
-    LibGLFW.focus_window(window)
+  @[AlwaysInline]
+  def self.focus_window(window : Window) : Nil
+    LibGLFW.focus_window(window.ptr)
+  end
+
+  # fun get_window_monitor = glfwGetWindowMonitor(window : Window*) : Monitor*
+  @[AlwaysInline]
+  def self.get_window_monitor(window : Window) : Monitor?
+    ptr = LibGLFW.get_window_monitor(window.ptr)
+    if ptr.null?
+      nil
+    else
+      Monitor.new(ptr)
+    end
+  end
+
+  # fun poll_events = glfwPollEvents : Void
+  @[AlwaysInline]
+  def self.poll_events : Nil
+    LibGLFW.poll_events
+  end
+
+  # fun wait_events = glfwWaitEvents : Void
+  @[AlwaysInline]
+  def self.wait_events : Nil
+    LibGLFW.wait_events
+  end
+
+  # fun wait_events_timeout = glfwWaitEventsTimeout(timeout : Float64) : Void
+  @[AlwaysInline]
+  def self.wait_events_timeout(timeout : Float64) : Nil
+    LibGLFW.wait_events_timeout(timeout)
+  end
+
+  # fun post_empty_event = glfwPostEmptyEvent : Void
+  @[AlwaysInline]
+  def self.post_empty_event : Nil
+    LibGLFW.post_empty_event
+  end
+
+  # fun swap_buffers = glfwSwapBuffers(window : Window*) : Void
+  @[AlwaysInline]
+  def self.swap_buffers(window : Window) : Nil
+    LibGLFW.swap_buffers(window.ptr)
   end
 end
