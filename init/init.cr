8704c3d2e6f5a2bca065133e8c2ee32b7c2d6d1a
Heaven31415 <Heaven31415@gmail.com>
2018-08-25 18:13:36 +0200

    Inline init methods.

--- init/init.cr
+++ init/init.cr
@@ -1,29 +1,39 @@
 module GLFW
+  # fun init = glfwInit : Int32
+  @[AlwaysInline]
   def self.init : Bool
     LibGLFW.init == LibGLFW::TRUE ? true : false
   end
 
-  def self.terminate
+  # fun terminate = glfwTerminate : Void
+  @[AlwaysInline]
+  def self.terminate : Nil
     LibGLFW.terminate
   end
 
+  # fun get_version = glfwGetVersion(major : Int32*, minor : Int32*, rev : Int32*) : Void
+  @[AlwaysInline]
   def self.get_version : {major: Int32, minor: Int32, rev: Int32}
     LibGLFW.get_version(out major, out minor, out rev)
     {major: major, minor: minor, rev: rev}
   end
 
+  # fun get_version_string = glfwGetVersionString : UInt8*
+  @[AlwaysInline]
   def self.get_version_string : String
     String.new(LibGLFW.get_version_string)
   end
 
+  # fun set_error_callback = glfwSetErrorCallback(cbfun : ErrorFun) : ErrorFun
   @@error_callback : Proc(Error, String, Void)? = nil
-  def self.set_error_callback(&block : Error, String -> Void)
+  @[AlwaysInline]
+  def self.set_error_callback(&block : Error, String -> Void) : Proc(Error, String, Void)?
     @@error_callback = block
-
     LibGLFW.set_error_callback ->(error : Int32, description : UInt8*) do
       if cb = @@error_callback
         cb.call(Error.new(error), String.new(description))
       end
     end
+    @@error_callback
   end
 end
