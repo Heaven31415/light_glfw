6221b1f3ef95ca303cc8787c854afd3c15a4eaa1
Heaven31415 <Heaven31415@gmail.com>
2018-08-29 17:08:45 +0200

    Fix init and monitor tests.

--- test/init.cr
+++ test/init.cr
@@ -1,4 +1,8 @@
-require "../glfw"
+require "../src/light_glfw"
+
+GLFW.set_error_callback do |error, string|
+  puts "Error: `#{error}` description: `#{string}`"
+end
 
 version = GLFW.get_version
 puts "Major: #{version[:major]}, Minor: #{version[:minor]}, Revision: #{version[:rev]}"
@@ -6,13 +10,6 @@ puts "Major: #{version[:major]}, Minor: #{version[:minor]}, Revision: #{version[
 version_string = GLFW.get_version_string
 puts "Version String: `#{version_string}`"
 
-GLFW.set_error_callback do |error, description|
-  raise "Error: #{error} => #{description}"
+if GLFW.init
+  GLFW.terminate
 end
-
-status = GLFW.init
-unless status == true
-  raise "Failed to initialize GLFW"
-end
-
-GLFW.terminate
--- test/monitor.cr
+++ test/monitor.cr
@@ -1,63 +1,64 @@
-require "../glfw"
+require "../src/light_glfw"
 
-status = GLFW.init
-unless status == true
-  raise "Failed to initialize GLFW"
+GLFW.set_error_callback do |error, string|
+  puts "Error: `#{error}` description: `#{string}`"
 end
 
-monitors = GLFW.get_monitors
-puts "#{monitors.size} monitors have been found"
+if GLFW.init
+  monitors = GLFW.get_monitors
+  puts "monitors: #{monitors}"
 
-primary = GLFW.get_primary_monitor
-puts "Primary monitor: #{primary}"
+  primary_monitor = GLFW.get_primary_monitor
+  puts "primary_monitor: #{primary_monitor}"
 
-position = GLFW.get_monitor_pos(primary)
-puts "Primary monitor x: #{position[:x]}, y: #{position[:y]}"
+  position = GLFW.get_monitor_pos(primary_monitor)
+  puts "primary_monitor x: #{position[:x]} y: #{position[:y]}"
 
-physical_size = GLFW.get_monitor_physical_size(primary)
-puts "Primary monitor physical size width: #{physical_size[:width]}mm, height: #{physical_size[:height]}mm"
+  physical_size = GLFW.get_monitor_physical_size(primary_monitor)
+  puts "primary_monitor width: #{physical_size[:width]}mm height: #{physical_size[:height]}mm"
 
-name = GLFW.get_monitor_name(primary)
-puts "Primary monitor name: #{name}"
+  name = GLFW.get_monitor_name(primary_monitor)
+  puts "primary_monitor name: #{name}"
 
-GLFW.set_monitor_callback do |monitor, event|
-  case event
-  when GLFW::Event::Monitor::Connected
-    puts "Monitor #{monitor} has been connected"
-  when GLFW::Event::Monitor::Disconnected
-    puts "Monitor #{monitor} has been disconnected"
+  GLFW.set_monitor_callback do |monitor, event|
+    case event
+    when GLFW::Event::Connected
+      puts "monitor #{monitor} has been connected"
+    when GLFW::Event::Disconnected
+      puts "monitor #{monitor} has been disconnected"
+    end
   end
+
+  video_modes = GLFW.get_video_modes(primary_monitor)
+  video_modes.each { |video_mode| puts "video_mode: #{video_mode}" }
+
+  video_mode = GLFW.get_video_mode(primary_monitor)
+  puts "video_mode: #{video_mode}"
+
+  # TODO: find a good value for testing
+  # GLFW::Monitor.set_gamma(primary, 0.0f32)
+
+  gamma_ramp = GLFW.get_gamma_ramp(primary_monitor)
+  puts "gamma_ramp size: #{gamma_ramp.size}"
+
+  puts "gamma_ramp red:"
+  20.times do |i|
+    print "#{gamma_ramp.red[i]} "
+    puts if i == 19
+  end
+
+  puts "gamma_ramp green:"
+  20.times do |i|
+    print "#{gamma_ramp.green[i]} "
+    puts if i == 19
+  end
+
+  puts "gamma_ramp blue:"
+  20.times do |i|
+    print "#{gamma_ramp.blue[i]} "
+    puts if i == 19
+  end
+
+  GLFW.set_gamma_ramp(primary_monitor, gamma_ramp)
+  GLFW.terminate
 end
-
-video_modes = GLFW.get_video_modes(primary)
-puts "Found #{video_modes.size} video modes for #{primary}"
-video_modes.each do |video_mode|
-  puts "Video mode: #{video_mode}"
-end
-
-# I'm unable to find correct gamma value, however it must be greater than zero
-# GLFW::Monitor.set_gamma(primary, 0.0f32)
-
-gamma_ramp = GLFW.get_gamma_ramp(primary)
-puts "Gamma Ramp size: #{gamma_ramp.size}"
-puts "Gamma Ramp red:"
-20.times do |i|
-  print "#{gamma_ramp.red[i]} "
-  puts if i == 19
-end
-
-puts "Gamma Ramp green:"
-20.times do |i|
-  print "#{gamma_ramp.green[i]} "
-  puts if i == 19
-end
-
-puts "Gamma Ramp blue:"
-20.times do |i|
-  print "#{gamma_ramp.blue[i]} "
-  puts if i == 19
-end
-
-GLFW.set_gamma_ramp(primary, gamma_ramp)
-
-GLFW.terminate
