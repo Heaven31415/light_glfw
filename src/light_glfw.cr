06a861447188d18da9d259e81d0e0a635143f9d2
Heaven31415 <Heaven31415@gmail.com>
2018-09-15 08:31:13 +0200

    Add compile-time check for GLFW3 existence with correct version.

--- src/light_glfw.cr
+++ src/light_glfw.cr
@@ -7,7 +7,19 @@ require "./input/*"
 require "./monitor/*"
 require "./window/*"
 
-@[Link("glfw")]
+{% begin %}
+  LibGLFW::MINIMUM = "3.2.1"
+  LibGLFW::EXISTS  = {{ `command -v pkg-config > /dev/null && pkg-config --exists glfw3 || printf %s false`.stringify != "false" }}
+  LibGLFW::VERSION = {{ `pkg-config --modversion --silence-errors glfw3 || printf %s 0.0.0`.stringify }}
+{% end %}
+
+{% if !LibGLFW::EXISTS %}
+  raise "Unable to find installed GLFW3 library on your system"
+{% elsif compare_versions(LibGLFW::VERSION, LibGLFW::MINIMUM) == -1 %}
+  raise "Your GLFW3 library is too old. You have {{LibGLFW::VERSION.id}} version, but need {{LibGLFW::MINIMUM.id}}"
+{% else %}
+  @[Link("glfw")]
+{% end %}
 lib LibGLFW
 end
 
