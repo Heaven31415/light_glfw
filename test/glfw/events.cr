f4767517f71cc27d2e8fe0466b15e3dd8878c5fb
Heaven31415 <Heaven31415@gmail.com>
2018-10-21 08:41:56 +0200

    Start implementing GLFW events test.

--- test/glfw/events.cr
+++ test/glfw/events.cr
@@ -34,26 +34,78 @@
 
 require "../utility"
 
-counter = 0u32
+module Test
+  @@counter = 0u32
 
-struct Slot
-  property window : GLFW::Window
-  property number : Int32
-  property closeable : Bool
+  struct Slot
+    property window : GLFW::Window
+    property number : Int32
+    property closeable : Bool
 
-  def initialize(@window, @number, @closeable)
+    def initialize(@window, @number, @closeable)
+    end
   end
-end
 
-usage = ->(code : Int32) do
-  puts "Usage: events [-f] [-h] [-n WINDOWS]"
-  puts "Options:"
-  puts "  -f use full screen"
-  puts "  -h show this help"
-  puts "  -n the number of windows to create"
-  exit(code)
-end
+  def self.usage(code : Int32)
+    puts "Usage: events [-f] [-h] [-n WINDOWS]"
+    puts "Options:"
+    puts "  -f use full screen"
+    puts "  -h show this help"
+    puts "  -n the number of windows to create"
+    exit(code)
+  end
 
-error_callback = ->(error : GLFW::Error, description : String) do
-  puts "Error(#{error}): #{description}"
+  def self.error_callback(error : GLFW::Error, description : String)
+  end
+
+  def self.window_pos_callback(window : GLFW::Window, x : Int32, y : Int32)
+  end
+
+  def self.window_size_callback(window : GLFW::Window, width : Int32, height : Int32)
+  end
+
+  def self.framebuffer_size_callback(window : GLFW::Window, width : Int32, height : Int32)
+  end
+
+  def self.window_close_callback(window : GLFW::Window)
+  end
+
+  def self.window_refresh_callback(window : GLFW::Window)
+  end
+
+  def self.window_focus_callback(window : GLFW::Window, focused : Bool)
+  end
+
+  def self.window_iconify_callback(window : GLFW::Window, iconified : Bool)
+  end
+
+  def self.mouse_button_callback(window : GLFW::Window, button : GLFW::MouseButton, action : GLFW::Action, mods : GLFW::Mod)
+  end
+
+  def self.cursor_position_callback(window : GLFW::Window, x : Float64, y : Float64)
+  end
+
+  def self.cursor_enter_callback(window : GLFW::Window, entered : Bool)
+  end
+
+  def self.scroll_callback(window : GLFW::Window, dx : Float64, dy : Float64)
+  end
+
+  def self.key_callback(window : GLFW::Window, key : GLFW::Key, scancode : Int32, action : GLFW::Action, mods : GLFW::Mod)
+  end
+
+  def self.char_callback(window : GLFW::Window, char : Char)
+  end
+
+  def self.char_mods_callback(window : GLFW::Window, char : Char, mods : GLFW::Mod)
+  end
+
+  def self.drop_callback(window : GLFW::Window, paths : Array(String))
+  end
+
+  def self.monitor_callback(monitor : GLFW::Monitor, event : GLFW::Event)
+  end
+
+  def self.joystick_callback(joy : GLFW::Joystick, event : GLFW::Event)
+  end
 end
